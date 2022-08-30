6edf9ee500431ab9ebb1e6c769256e925d851d08
Aurelien Pellegrini <aurelien.pellegrini@gmail.com>
2022-08-30 19:50:21 +0200

    anamnases shown in accordeon

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,13 +28,14 @@ tour = Tour.new(date: Date.today, user: nurse)
 tour.save!
 
 #generate patients
-1..3.times do
+1..8.times do
   p 'fake patient creation'
   profile_pic = URI.open(URL).read
   profile_pic_data = JSON.parse(profile_pic)
   patient_name_first = profile_pic_data["results"][0]["name"]["first"]
   patient_name_second = profile_pic_data["results"][0]["name"]["last"]
   patient_age = profile_pic_data["results"][0]["dob"]["date"]
+  patient_gender = profile_pic_data["results"][0]["gender"]
 
   profile_pic_url = profile_pic_data["results"][0]["picture"]["medium"]
   birth_date = Faker::Date.between(from: '1930-09-23', to: '1970-09-25')
@@ -44,7 +45,8 @@ tour.save!
     first_name: patient_name_first,
     last_name: patient_name_second,
     birth_date: patient_age,
-    profile_pic: profile_pic_url
+    profile_pic: profile_pic_url,
+    gender: patient_gender
   )
 
   patient.save!
@@ -56,7 +58,7 @@ tour.save!
   anamnesis = Anamnesis.new(
     patient: patient,
     date:  Faker::Date.between(from: birth_date, to: Date.today),
-    description: table[rand(0..50)]
+    description: table[rand(0..50)][0].strip
   )
   anamnesis.save!
 end
