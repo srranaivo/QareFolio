fabcea7055768509559e2ab475a168901942922d
Aurelien Pellegrini <aurelien.pellegrini@gmail.com>
2022-08-30 12:10:29 +0200

    seed and tour show

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,2 +1,10 @@
 class ToursController < ApplicationController
+  def index
+    @tours = Tour.all
+  end
+
+  def show
+    @tour = Tour.find(params[:id])
+    @consultations = @tour.consultations
+  end
 end
