6446d8b969ccd2cf18afdebdbc3ab4d2ec53c109
Aurelien Pellegrini <aurelien.pellegrini@gmail.com>
2022-08-30 15:26:01 +0200

    seed and profile patient

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :tours
+  resources :consultations, only: [:show]
 end
