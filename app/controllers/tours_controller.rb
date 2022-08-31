0fc42c1a58ae0c136161539f04aca8d987e68928
Aurelien Pellegrini <aurelien.pellegrini@gmail.com>
2022-08-31 15:30:30 +0200

    create google map api request

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,3 +1,7 @@
+require "erb"
+require "uri"
+require "net/http"
+
 class ToursController < ApplicationController
   def index
     @tours = Tour.all
@@ -6,5 +10,26 @@ class ToursController < ApplicationController
   def show
     @tour = Tour.find(params[:id])
     @consultations = @tour.consultations
+  end
+
+  def map
+    base_url = "https://maps.googleapis.com/maps/api/directions/json?"
+    origin = url_encode("Renens, route de lausanne 64")
+    destination = url_encode("Vevey")
+    waypoints = url_encode("Yverdon-les-bains,VD")
+    api_key = 'AIzaSyCbwW6MnwhGlDzRcXt60O9uzirzV_1O3E4'
+
+    url_encoded = "#{base_url}origin=#{origin}&destination=#{destination}&waypoints=#{waypoints}&key=#{api_key}"
+
+    url = URI(url_encoded)
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+
+    response = https.request(request)
+    puts response.read_body
+    p url_encoded
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :tours
+
+
+  resources :tours do
+    get "map", to: "tours#map"
+  end
   resources :consultations, only: [:show]
 end
